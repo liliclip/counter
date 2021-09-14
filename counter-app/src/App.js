32dfafd941ba5092a115095934f46b5b06be69e0
Lili <lilia.ramirez@payclip.com>
2021-09-14 13:19:40 -0500

    Prueba3

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>¡Hola soy Lili!</p>
+        <p>¡Hola Mundo!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
